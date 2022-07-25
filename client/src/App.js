7a09bbab6ec8c286b8911646370734986ef1ba53
James Jewitt <jamestjewitt@gmail.com>
2022-07-25 15:48:20 -0400

    created React Client

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+      </header>
+      <main>
+        <Routes>
+          <Route/>
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
