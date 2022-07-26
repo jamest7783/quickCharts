aba4b4ae20cb18c2976700a542275d4a2bcfdcd7
James Jewitt <jamestjewitt@gmail.com>
2022-07-25 22:52:31 -0400

    made button navigate to Library in Searach component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Search = (  ) => {
 
+    const navigate = useNavigate()
+    const routeChange = (  ) => {
+        let path = '/library'
+        navigate( path )
+        //will have to bring input with route change
+    }
+
     return (
         <div>
-            <Link to='/library'>?</Link>
             <form>
+                <button onClick={ routeChange }>onSubmit link to Library with search input</button>
                 <input placeholder='search by data or axis'>
                 
                 </input>
