38396510486b45b3dc5e26edeb8c38fc00fd92fa
James Jewitt <jamestjewitt@gmail.com>
2022-07-29 09:21:52 -0400

    Initial Mongo Atlas config formatting

--- db/index.js
+++ db/index.js
@@ -1,10 +1,30 @@
-const mongoose = require( 'mongoose' )
+// const mongoose = require( 'mongoose' )
 
-mongoose.connect( 'mongodb://127.0.0.1:27017/dataDb' ).then( ( ) => {
-    console.log( 'Successfully connected to MongoDB' )
-}).catch( ( e ) => {
-    console.log( 'Connection Error', e.message ) 
-})
+// mongoose.connect( 'mongodb://127.0.0.1:27017/dataDb' ).then( ( ) => {
+//     console.log( 'Successfully connected to MongoDB' )
+// }).catch( ( e ) => {
+//     console.log( 'Connection Error', e.message ) 
+// })
 
+// const db = mongoose.connection
+// module.exports = db 
+
+
+
+const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
+
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/<database_name>'
+
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log('Successfully connected to MongoDB!')
+  })
+  .catch((e) => {
+    console.error('Connection error', e.message)
+  })
+mongoose.set('debug', true)
 const db = mongoose.connection
-module.exports = db 
+
+module.exports = db
